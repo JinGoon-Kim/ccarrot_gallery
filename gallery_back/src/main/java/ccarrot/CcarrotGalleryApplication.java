a0925670d3f51d68109a8e88ce75e2093ff8f2b2
JinGoon-Kim <raneata@gmail.com>
2021-10-08 22:51:59 +0900

    initial commit

---
+++ gallery_back/src/main/java/ccarrot/CcarrotGalleryApplication.java
@@ -0,0 +1,13 @@
+package ccarrot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcarrotGalleryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcarrotGalleryApplication.class, args);
+    }
+
+}
---
+++ gallery_back/src/test/java/ccarrot/CcarrotGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package ccarrot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcarrotGalleryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
