32a4c1ed4cd12e6216f7c48139105dbf5bf7d90a
ChanGeunPark <design795@naver.com>
2021-10-18 22:20:01 +0900

    fix : form 오류수정

--- gallery_front/gallery_app/src/App.js
+++ gallery_front/gallery_app/src/App.js
@@ -11,35 +11,38 @@ function App() {
   //useState는 항상 두개의 데이터를 준다.
 
   //useState는 사이트내에서 바뀔때 새로고침없이 재 랜더링 해준다. 변경이 될 데이터들을 state를 만들어서 넣어준다.
-  let [글제목, 글제목변경] = useState('HIHI');
+  let [_get, get변경] = useState('hellow Word _get');
+  let [_put, put변경] = useState('hellow Word _put');
+  let [_delete, delete변경] = useState('hellow Word _delete');
+  let [_post, post변경] = useState('hellow Word _post');
 
   return (
     <div className="App">
-      <header className="nav">
-        CCARROT
+      <header>
+        <h1>CCARROT</h1>
+        <nav>
+          
+        </nav>
       </header>
-      <h3 className="text">
-        { 글제목 }
-      </h3>
       
       <section class="form_test">
         <form action="localhost:8070/api_test" method="get" class="form_example_get">
-          <input type="text" value="hellow word! get">
+          <input value={_get}/>
           <button class="request_button" type="button">GET</button>
         </form>
 
         <form action="localhost:8070/api_test" method="put" class="form_example_put">
-        <input type="text" value="hellow word! put">
+          <input value={_put}/>
           <button class="request_button" type="button">PUT</button>
         </form>
 
         <form action="localhost:8070/api_test" method="delete" class="form_example_delete">
-          <input type="text" value="hellow word! delete">
+          <input value={_delete}/>
           <button class="request_button" type="button">DELETE</button>
         </form>
 
         <form action="localhost:8070/api_test" method="post" class="form_example4_post">
-          <input type="text" value="hellow word! post">
+          <input value={_post}/>
           <button class="request_button" type="button">POST</button>
         </form>
       </section>
