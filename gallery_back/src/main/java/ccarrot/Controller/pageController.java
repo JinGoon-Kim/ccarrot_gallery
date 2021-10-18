5571bf87e3994c1349c2c794a5052892723c880f
JinGoon-Kim <raneata@gmail.com>
2021-10-18 22:01:39 +0900

    test RestApi call

--- gallery_back/src/main/java/ccarrot/Controller/pageController.java
+++ gallery_back/src/main/java/ccarrot/Controller/pageController.java
@@ -1,9 +1,28 @@
 package ccarrot.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
 public class pageController {
 
+    @GetMapping("api_test")
+    public String get_test() {
+        return "GetMapping_ok";
+    }
+    @PostMapping("api_test")
+    public String post_test() {
+        return "PostMapping_ok";
+    }
+    @PutMapping("api_test")
+    public String put_test() {
+        return "PutMapping_ok";
+    }
+    @DeleteMapping("api_test")
+    public String delete_test() {
+        return "DeleteMapping_ok";
+    }
 }
--- gallery_back/src/main/java/ccarrot/member/Member.java
+++ gallery_back/src/main/java/ccarrot/member/Member.java
@@ -4,12 +4,13 @@ import lombok.Data;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Entity
+@Data
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name")
@@ -28,22 +29,5 @@ public class Member {
 
     @Lob
     private String description;
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
 
