7af02035d361e38c5623a2ccbf122db2be3a0276
JinGoon-Kim <raneata@gmail.com>
2021-12-07 20:36:16 +0900

    fixed: CORS setting

--- gallery_back/src/main/java/ccarrot/Api/WebConfig.java
+++ gallery_back/src/main/java/ccarrot/Api/WebConfig.java
@@ -10,8 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
-        registry.addMapping("/**")
-                .allowedOrigins("http://182.226.132.182:3000");
+                .allowedOrigins("http://localhost:3000", "http://182.226.132.182:3000");
     }
 }
