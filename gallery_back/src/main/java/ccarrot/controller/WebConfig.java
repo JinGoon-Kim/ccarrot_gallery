d7df4e4e87b472ef576ce41cd45c57b53e1e793a
JinGoon-Kim <raneata@gmail.com>
2022-02-03 21:53:05 +0900

    fixed CORS

--- gallery_back/src/main/java/ccarrot/controller/WebConfig.java
+++ gallery_back/src/main/java/ccarrot/controller/WebConfig.java
@@ -11,9 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "/**"
-                );
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 
     @Override
--- gallery_back/src/main/java/ccarrot/controller/api/v1/FileApiController.java
+++ gallery_back/src/main/java/ccarrot/controller/api/v1/FileApiController.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
